4190c64eac5ca9e20306d46999e38cc3c5f7be9b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-06-30 00:31:59 +0900

    wip Creatとプレビュー

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Deno のフレームワーク Fresh1.0 を利用
 
 - DOM 操作：Preact(React のより軽量なもの)
 - CSS フレームワーク：twind （ビルド不要な Tailwind てきなもの)
+
+https://zenn.dev/azukiazusa/articles/fresh-tutorial
+
+https://zenn.dev/k41531/articles/69c9342f7022dd
--- deno_fresh/routes/index.tsx
+++ deno_fresh/routes/index.tsx
@@ -5,7 +5,7 @@ import { Handlers } from "$fresh/server.ts";
 import { tw } from "@twind";
 
 // dayjs で相対日付を表示するために import
-import dayjs from "https://esm.sh/dayjs@1.11.3";
+import dayjs from "dayjs/";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ja";
 // カスタムハンドラー
@@ -47,16 +47,28 @@ export default function Home({ data }: PageProps<Article[]>) {
   return (
     <div class={tw("h-screen bg-gray-200")}>
       <Head>
-        <title>Fresh Blog</title>
+        <title>Usuyuki Blog</title>
       </Head>
       <div
         class={tw(
           "max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col"
         )}
       >
-        <h1 class={tw("font-extrabold text-5xl text-gray-800")}>Fresh Blog</h1>
+        <h1 class={tw("font-extrabold text-5xl text-gray-800")}>
+          Usuyuki Blog
+        </h1>
         <section class={tw("mt-8")}>
-          <h2 class={tw("text-4xl font-bold text-gray-800 py-4")}>Posts</h2>
+          <div class={tw("flex justify-between items-center")}>
+            <h2 class={tw("text-4xl font-bold text-gray-800 py-4")}>Posts</h2>
+            <a
+              href="/articles/create"
+              class={tw(
+                "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+              )}
+            >
+              Create Post
+            </a>
+          </div>
           <ul>
             {data.map((article) => (
               <li
