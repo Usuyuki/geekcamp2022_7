a520b1b569099bfe9a1f47810da6b7458212f7f6
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-07-04 22:51:48 +0900

    feat denoもdocker化

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
     router := gin.Default()
 
-    router.GET("/", func(c *gin.Context) {
+    router.GET("/api", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "技育博用のdocker整備！！!!",
         })
