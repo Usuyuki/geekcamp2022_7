2b666a13825235d26d5e9bb21182f81481706984
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-07-09 09:26:31 +0900

    fix カラーパレット変更とfavicon追加

--- frontend/utils/twind.ts
+++ frontend/utils/twind.ts
@@ -1,8 +1,19 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
+import * as colors from "twind/colors";
+
 export const config: Configuration = {
   darkMode: "class",
   mode: "silent",
+  theme: {
+    colors: {
+      musubiberu_1: "#e1f7ac",
+      musubiberu_2: "#cfc6f7",
+      musubiberu_3: "#baf7ba",
+      musubiberu_4: "#f794c6",
+      musubiberu_5: "#f7eda1",
+    },
+  },
 };
 if (IS_BROWSER) setup(config);
